322a8a843013f8fbd2aed99ea8807ebe3552c467
wenderson1 <wenderson@alunos.fho.edu.br>
2021-06-17 23:05:27 -0300

    Lacos de Repeticao

---
+++ Estrutura_Condicoes/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Estrutura_Condicoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           #region Estrutura de Condicao IF
+            var notaDigitada = Console.ReadLine();
+            var nota = int.Parse(notaDigitada);
+
+            if (nota >= 70)
+            {
+                Console.WriteLine("Aprovado");
+            }
+            else if(nota >= 40)
+            {
+                Console.WriteLine("Em Recuperação");
+            }
+            else
+            {
+                Console.WriteLine("Reprovado");
+            }
+            #endregion
+
+           #region Estrutura de Condicao Switch-Case
+            Console.WriteLine("Seja bem-vindo a empresa Xcom");
+            Console.WriteLine("Digite 1 - Contratação de um plano");
+            Console.WriteLine("Digite 2 - Reclamação de serviço prestado");
+            Console.WriteLine("Digite 3 - Segunda Via de Boleto");
+            Console.WriteLine("Digite 4 - Sair");
+
+            var opcao = Console.ReadLine();
+
+            switch(opcao)
+            {
+                case "1":
+                    Console.WriteLine("Informações do Plano Novo");
+                    break;
+
+                case "2":
+                    Console.WriteLine("Fale sobre sua reclamação");
+                    break;
+
+                case "3":
+                    Console.WriteLine("Segunda via de boleto enviada por e-mail");
+                    break;
+
+                case "4":
+                    Console.WriteLine("Tenha um bom dia");
+                    break;
+
+                default:
+                    Console.WriteLine("Opcao Invalida");
+                    break;
+            }
+            #endregion
+        }
+    }
+}
---
+++ Estrutura_Repeticao/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Estrutura_Repeticao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Digite uma sequência de números separados por espaço");
+
+            var numerosTexto = Console.ReadLine();
+
+            var numeros = numerosTexto.Split(' ');
+            Console.WriteLine("Numeros: ");
+
+            #region Laço de repetição - For
+            Console.WriteLine("Usando For");
+            for (int i=0;i < numeros.Length; i++)
+            {
+                Console.WriteLine(numeros[i]);
+            }
+            #endregion
+
+            #region Laço de repetição - While
+            var contador = 0;
+            Console.WriteLine("Numeros: ");
+            Console.WriteLine("Usando Foreach");
+            while (contador < numeros.Length)
+            {
+                Console.WriteLine(numeros[contador]);
+                contador++;
+            }
+            #endregion
+
+            #region Laço de repetição - foreach
+           
+            Console.WriteLine("Numeros: ");
+            Console.WriteLine("Usando Foreach");
+            foreach(var x in numeros)
+            {
+                Console.WriteLine(x);
+            }
+            #endregion
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
