fa0b3147612f0a4bc2940ee8c7444976d7822f1f
wenderson1 <wenderson@alunos.fho.edu.br>
2021-06-26 17:32:03 -0300

    Debugando Aplicações

---
+++ DebugandoAplicacoes/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DebugandoAplicacoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //f10 para ir passando para a frente
+            var numerosString = Console.ReadLine();
+            var numeros = numerosString.Split(' '); //se estiver errado, posso testar mudando o valor da váriavel e avançar com o código
+
+            foreach(var numero in numeros)
+            {
+                var numeroInt = int.Parse(numero);
+
+                var aoQuadrado = Math.Pow(numeroInt, 2);
+
+                Console.WriteLine($"{numeroInt}2 = {aoQuadrado}");
+
+            }
+        }
+    }
+}
---
+++ Tratamento_Excecoes/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Tratamento_Excecoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
