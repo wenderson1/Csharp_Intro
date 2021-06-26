2ae9c117a19c70e63db2236c43a6162e0c2d8484
wenderson1 <wenderson@alunos.fho.edu.br>
2021-06-26 17:45:10 -0300

    Tratamento de Excecoes

--- Tratamento_Excecoes/Program.cs
+++ Tratamento_Excecoes/Program.cs
@@ -6,6 +6,37 @@ namespace Tratamento_Excecoes
     {
         static void Main(string[] args)
         {
+            var seteString = "sete";
+            string valorNull = null;
+            var longValue = long.MaxValue.ToString();
+
+            try
+            {
+               // var formatException = int.Parse(seteString);
+                //var argumentNullException = int.Parse(valorNull);
+                var overflowException = int.Parse(longValue);
+            }
+            catch(FormatException exception)
+            {
+                Console.WriteLine("Format Exception:");
+                Console.WriteLine(exception);
+                Console.WriteLine(exception.Message);
+            }
+            catch(ArgumentNullException exception)
+            {
+                Console.WriteLine("Argument Null Exception");
+                Console.WriteLine(exception.Message);
+            }
+            catch(OverflowException exception)
+            {
+                Console.WriteLine("Overflow Exception");
+                Console.WriteLine(exception);
+                Console.WriteLine(exception.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Este código sempre é executado");
+            }
         }
     }
 }
